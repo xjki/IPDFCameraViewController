f7f21a5f3c1d2a43e9684a4565ca69ecf2816a5e
jki <jki@jki.lv>
2016-09-15 12:36:59 +0300

    Added workaround for issue on iOS10 (portrait photos returned in landscape) - https://github.com/mmackh/IPDFCameraViewController/issues/27

--- IPDFCameraViewController/IPDFCameraViewController.m
+++ IPDFCameraViewController/IPDFCameraViewController.m
@@ -476,7 +476,20 @@ void saveCGImageAsJPEGToFilePath(CGImageRef imageRef, NSString *filePath)
     rectangleCoordinates[@"inputTopRight"] = [CIVector vectorWithCGPoint:rectangleFeature.topRight];
     rectangleCoordinates[@"inputBottomLeft"] = [CIVector vectorWithCGPoint:rectangleFeature.bottomLeft];
     rectangleCoordinates[@"inputBottomRight"] = [CIVector vectorWithCGPoint:rectangleFeature.bottomRight];
-    return [image imageByApplyingFilter:@"CIPerspectiveCorrection" withInputParameters:rectangleCoordinates];
+    image = [image imageByApplyingFilter:@"CIPerspectiveCorrection" withInputParameters:rectangleCoordinates];
+
+    // Fix landscape orientiation in iOS 10 by rotating image
+    if ([[[UIDevice currentDevice] systemVersion] compare:@"10.0" options:NSNumericSearch] != NSOrderedAscending) {
+        CIFilter *transform = [CIFilter filterWithName:@"CIAffineTransform"];
+        [transform setValue:image forKey:kCIInputImageKey];
+        NSValue *rotation = [NSValue valueWithCGAffineTransform:CGAffineTransformMakeRotation((CGFloat) (-90 * (M_PI / 180)))];
+        if ([transform respondsToSelector:@selector(setValue:forKey:)]) {
+            [transform setValue:rotation forKey:[NSString stringWithFormat:@"inputTransform"]];
+        }
+        image = [transform outputImage];
+    }
+
+    return image;
 }
 
 - (CIDetector *)rectangleDetetor
